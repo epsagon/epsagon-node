607604323ae5a2a88c1787f3942f687751c9327c
Ran Ribenzaft <ranrib@users.noreply.github.com>
2018-10-14 18:00:43 +0300

    Add aws account to the Lambda runner event

--- src/runners/aws_lambda.js
+++ src/runners/aws_lambda.js
@@ -31,10 +31,14 @@ function createRunner(originalContext, resourceType = 'lambda') {
     ]);
 
     runner.setResource(runnerResource);
+    const AWSAccountNumber = (originalContext.invokedFunctionArn) ?
+        originalContext.invokedFunctionArn.split(':')[4] : '';
+
     eventInterface.addToMetadata(runner, {
         log_stream_name: `${originalContext.logStreamName}`,
         log_group_name: `${originalContext.logGroupName}`,
         function_version: `${originalContext.functionVersion}`,
+        aws_account: `${AWSAccountNumber}`,
         cold_start: `${consts.COLD_START}`,
         memory: `${originalContext.memoryLimitInMB}`,
         region: consts.REGION,
