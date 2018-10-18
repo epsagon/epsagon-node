5c36a977e4a461a385bf1cdc88d1351d05ca4bfa
Ilya Libin <libinsaas@gmail.com>
2018-10-18 10:22:53 +0300

    feat(instrumentation): adding pg instrumentation (#7)

---
+++ src/events/pg.js
@@ -0,0 +1,117 @@
+const uuid4 = require('uuid4');
+const shimmer = require('shimmer');
+const pg = require('pg');
+const Pool = require('pg-pool');
+const sqlParser = require('node-sqlparser');
+const utils = require('../utils.js');
+const tracer = require('../tracer.js');
+const serverlessEvent = require('../proto/event_pb.js');
+const eventInterface = require('../event.js');
+const errorCode = require('../proto/error_code_pb.js');
+
+/**
+ * Wraps the pg's module request function with tracing
+ * @param {Function} wrappedFunction The pg's module
+ * @returns {Function} The wrapped function
+ */
+function pgClientWrapper(wrappedFunction) {
+    return function internalpgClientWrapper(queryString, arg1, arg2) {
+        let patchedCallback;
+        const callback = (arg2 === undefined) ? arg1 : arg2;
+        const params = (arg1 instanceof Function) ? [] : arg1;
+        let sqlObj = {};
+
+        try {
+            sqlObj = sqlParser.parse(queryString);
+        } catch (error) {
+            sqlObj.type = 'Can not process';
+            sqlObj.table = 'Can not process';
+        }
+
+        let databaseType = 'local';
+
+        const {
+            type,
+            table,
+        } = sqlObj;
+
+        const {
+            database,
+            host,
+        } = this.connectionParameters || this._clients[0]; // eslint-disable-line
+
+        if (host.match('.rds.')) { databaseType = 'rds'; }
+        if (host.match('.redshift.')) { databaseType = 'redshift'; }
+
+        try {
+            const resource = new serverlessEvent.Resource([
+                database, // name of the database
+                databaseType,
+                queryString,
+            ]);
+
+            const startTime = Date.now();
+
+            const dbapiEvent = new serverlessEvent.Event([
+                `dbapi-${uuid4()}`,
+                utils.createTimestampFromTime(startTime),
+                null,
+                'pg',
+                0,
+                errorCode.ErrorCode.OK,
+            ]);
+
+            dbapiEvent.setResource(resource);
+
+            const extendedData = {};
+            if ((type === 'select') || (!tracer.metadata_only)) {
+                extendedData.Query = queryString;
+            }
+
+            eventInterface.addToMetadata(dbapiEvent, {
+                Host: host,
+                Driver: 'pg',
+                Type: type,
+                'Table Name': table,
+            }, extendedData);
+
+
+            const responsePromise = new Promise((resolve) => {
+                patchedCallback = (err, res) => {
+                    if (err) {
+                        eventInterface.setException(dbapiEvent, err);
+                    } else {
+                        eventInterface.addToMetadata(dbapiEvent, {
+                            rowCount: res.rowCount,
+                        });
+                    }
+
+                    if (callback) {
+                        callback(err, res);
+                    }
+
+                    dbapiEvent.setDuration(utils.createDurationTimestamp(startTime));
+                    resolve();
+                };
+            }).catch((err) => {
+                tracer.addException(err);
+            });
+
+            tracer.addEvent(dbapiEvent, responsePromise);
+        } catch (error) {
+            tracer.addException(error);
+        }
+
+        return wrappedFunction.apply(this, [queryString, params, patchedCallback]);
+    };
+}
+
+module.exports = {
+    /**
+     * Initializes the pg tracer
+     */
+    init() {
+        shimmer.wrap(pg.Client.prototype, 'query', pgClientWrapper);
+        shimmer.wrap(Pool.prototype, 'query', pgClientWrapper);
+    },
+};
