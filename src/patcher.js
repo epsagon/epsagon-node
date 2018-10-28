0ca0f48910247afebebe4fb6f3c772725da03f44
Ilya Libin <ilya@epsagon.com>
2018-10-28 11:49:35 +0200

    fix(instrumentation): pg module not found

--- src/patcher.js
+++ src/patcher.js
@@ -2,17 +2,12 @@
  * @fileoverview Patcher for all the libraries we are instumenting
  * IMPORTANT: when requiring this module, all of the libraries will be automatically patched!
  */
+const tryRequire = require('try-require');
 const config = require('./config.js');
 const awsSDKPatcher = require('./events/aws_sdk.js');
 const httpPatcher = require('./events/http.js');
 
-let pgPatcher;
-
-try {
-    pgPatcher = require('./events/pg.js'); // eslint-disable-line
-} catch (err) {
-    pgPatcher = { init: () => {} };
-}
+const pgPatcher = tryRequire('./events/pg.js') || { init: () => {} };
 
 if (!config.config.isEpsagonPatchDisabled) {
     [awsSDKPatcher, httpPatcher, pgPatcher].forEach((patcher) => {
