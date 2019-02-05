b80bca7cd3c389ec145167b182b2ba6c6aca65b2
semantic-release (via TravisCI) <semantic-release@travis>
2019-02-05 09:39:09 +0200

    feat(layer): adding layer wrapper

---
+++ src/wrappers/lambda_env.js
@@ -0,0 +1,63 @@
+const tryRequire = require('try-require');
+const lambda = require('./lambda.js');
+
+/**
+ * @return {Function} the user's handler, or an error handler if an
+ *     error occurred.
+ */
+function getUserHandler() {
+    const createErrorHandler = err => ((event, context) => {
+        context.fail(err);
+    });
+
+    const createErrorHandlerWithMessage = (message, err) => ((event, context) => {
+        console.log(message); // eslint-disable-line no-console
+        context.fail(err);
+    });
+
+    const handlerString = process.env.EPSAGON_HANDLER;
+    if (!handlerString) {
+        return createErrorHandler(new Error(`invalid EPSAGON_HANDLER ${handlerString}`));
+    }
+
+    const appParts = handlerString.split('.');
+    if (appParts.length !== 2) {
+        return createErrorHandler(new Error(`Bad handler ${handlerString}`));
+    }
+
+    const modulePath = appParts[0];
+    const handlerName = appParts[1];
+    try {
+        const lambdaTaskRoot = process.env.LAMBDA_TASK_ROOT;
+        const moduleFullPath = `${lambdaTaskRoot}/${modulePath}`;
+        const app = tryRequire(moduleFullPath);
+        const userHandler = app[handlerName];
+
+        if (!userHandler) {
+            return createErrorHandler(
+                new Error(`Handler '${handlerName}' missing on module '${modulePath}'`)
+            );
+        }
+
+        return userHandler;
+    } catch (e) {
+        if (e.code === 'MODULE_NOT_FOUND') {
+            return createErrorHandlerWithMessage(
+                `Unable to import module '${modulePath}'`,
+                e
+            );
+        }
+        if (e instanceof SyntaxError) {
+            return createErrorHandlerWithMessage(
+                `Syntax error in module '${modulePath}'`,
+                e
+            );
+        }
+        return createErrorHandlerWithMessage(
+            'module initialization error',
+            e
+        );
+    }
+}
+
+module.exports.wrapper = lambda.lambdaWrapper(getUserHandler());
