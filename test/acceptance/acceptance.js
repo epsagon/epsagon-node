01eb1c78c4131bcfdc4b732ed0cc3df9924a674e
Erez Freiberger <erez@epsagon.com>
2019-03-10 14:42:31 +0200

    feat(acceptance): adding acceptance matrix

--- test/acceptance/acceptance.js
+++ test/acceptance/acceptance.js
@@ -1,4 +1,5 @@
 // vim: ts=4 sw=4 expandtab
+/* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const { expect } = require('chai');
@@ -6,7 +7,8 @@ const AWS = require('aws-sdk');
 
 chai.use(chaiAsPromised);
 
-const SERVICE_PREFIX = `epsagon-acceptance-node-${process.env.TRAVIS_BUILD_NUMBER}-dev-`;
+const SERVICE_PREFIX = `acceptance-node-${process.env.TRAVIS_BUILD_NUMBER}-dev-`;
+const RUNTIME = process.env.RUNTIME || 'nodejs8.10'
 
 /**
  * invokes a lambda
@@ -14,7 +16,7 @@ const SERVICE_PREFIX = `epsagon-acceptance-node-${process.env.TRAVIS_BUILD_NUMBE
  * @param {object} payload
  * @returns {hash} The functions output
  */
-async function invoke(name, payload) {
+function invoke(name, payload) {
     const lambda = new AWS.Lambda({ region: 'us-east-1' });
     const params = {
         FunctionName: SERVICE_PREFIX + name,
@@ -24,7 +26,37 @@ async function invoke(name, payload) {
 }
 
 /**
- * basicTest
+ * testSameOutput tests that the output of both normal and instrumented lambdas is the same
+ * instrumented lambdas are identified by having a '_e' suffix to the name
+ *
+ * @param {string} lambdaName
+ * @param {object} input
+ * @returns {undefined}
+ */
+async function testSameOutput(lambdaName, input) {
+    const responseOriginal = await invoke(lambdaName, input);
+    const responseInstrumented = await invoke(`${lambdaName}_e`, input);
+    expect(responseOriginal).to.eql(responseInstrumented);
+}
+
+/**
+ * basicTestNoInput - invokes a lambda and expects to get the input back
+ *
+ * @param {string} lambdaName
+ * @param {object} input
+ * @returns {undefined}
+ */
+async function basicTestNoInput(lambdaName, input) {
+    const response = await invoke(lambdaName, input);
+    expect(response.StatusCode).to.equal(200);
+    const content = JSON.parse(response.Payload);
+    expect(content.statusCode).to.equal(200);
+    const body = JSON.parse(content.body);
+    expect(body.message).to.eql('It Worked');
+}
+
+/**
+ * basicTest - invokes a lambda and expects to get the input back
  *
  * @param {string} lambdaName
  * @param {object} input
@@ -37,6 +69,32 @@ async function basicTest(lambdaName, input) {
     expect(content.statusCode).to.equal(200);
     const body = JSON.parse(content.body);
     expect(body.input).to.eql(input);
+}
+
+/**
+ * basicNoReturn
+ *
+ * @param {string} lambdaName
+ * @param {object} input
+ * @returns {undefined}
+ */
+async function basicNoReturn(lambdaName, input) {
+    const response = await invoke(lambdaName, input);
+    expect(response.StatusCode).to.equal(200);
+}
+
+/**
+ * failTest
+ *
+ * @param {string} lambdaName
+ * @param {object} input
+ * @returns {undefined}
+ */
+async function failTest(lambdaName, input) {
+    const response = await invoke(lambdaName, input);
+    expect(response.StatusCode).to.equal(200);
+    const content = JSON.parse(response.Payload);
+    expect(content.errorMessage).to.equal(input);
 }
 
 describe('Lambda Wrapper', () => {
@@ -55,7 +113,72 @@ describe('Lambda Wrapper', () => {
             await basicTest('sanity', input);
         });
         it('works with epsagon labels', async () => {
-            await basicTest('sanity', input);
+            await basicTest('labels', input);
+        });
+    });
+
+    const syncOptions = ['sync'];
+    const paramOptions = ['no', '1', '2', '3'];
+    const returnOptions = [
+        'simple_return', 'no_return',
+        'succeed', 'fail', 'done',
+        'callback', 'callback_error', 'callback_then_fail'];
+    if (RUNTIME === 'nodejs8.10') {
+        returnOptions.push('promise');
+    }
+    const testMatrix = {
+        sync: {
+            no: {
+                promise: basicTestNoInput,
+                simple_return: basicNoReturn,
+                no_return: basicNoReturn,
+            },
+            1: {
+                promise: basicTest,
+                simple_return: basicNoReturn,
+                no_return: basicNoReturn,
+            },
+            2: {
+                promise: basicTest,
+                simple_return: basicNoReturn,
+                no_return: basicNoReturn,
+                succeed: basicTest,
+                done: basicTest,
+                fail: failTest,
+            },
+            3: {
+                promise: basicTest,
+                simple_return: basicNoReturn,
+                no_return: basicNoReturn,
+                succeed: basicTest,
+                done: basicTest,
+                fail: failTest,
+                callback: basicTest,
+                callback_error: failTest,
+                // callback_then_fail: basicTest,
+            },
+        },
+        async: {
+            no: { },
+            1: { },
+            2: { },
+            3: { },
+        },
+    };
+
+    syncOptions.forEach((syncOpt) => {
+        paramOptions.forEach((paramOpt) => {
+            returnOptions.forEach((returnOpt) => {
+                const funcName = `${syncOpt}_${paramOpt}_param_${returnOpt}`;
+                if (testMatrix[syncOpt][paramOpt][returnOpt]) {
+                    it(`behaves correctly on ${funcName}`, async () => {
+                        await testSameOutput(funcName, 'hello world');
+                        await testMatrix[syncOpt][paramOpt][returnOpt](
+                            `${funcName}_e`, 'hello world'
+                        );
+                    });
+                }
+            });
         });
     });
 });
--- test/acceptance/run.sh
+++ test/acceptance/run.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+function cleanup() {
+    echo 'cleaning up'
+    cd /tmp/acceptance/lambda-handlers/
+    serverless remove --runtime ${runtime} --buildNumber ${build_num}
+    cd $INITIAL_PWD
+    rm -rf /tmp/acceptance
+}
+
+trap clenaup 1 2 15
+
 function run_acceptance_test() {
     build_num=$1
     runtime=$2
@@ -9,13 +19,11 @@ function run_acceptance_test() {
     npm install epsagon
     serverless deploy --runtime ${runtime} --buildNumber ${build_num} || {  echo "deployment of ${runtime} [build: ${build_num}] failed" ; result=1; }
     cd -
-    TRAVIS_BUILD_NUMBER=${build_num} mocha test/acceptance/acceptance.js || {  echo "tests ${runtime} [build: ${build_num}] failed" ; result=1; }
-    cd /tmp/acceptance/lambda-handlers/
-    serverless remove --runtime ${runtime} --buildNumber ${build_num}
-    cd -
-    rm -rf /tmp/acceptance
+    TRAVIS_BUILD_NUMBER=${build_num} RUNTIME=${runtime} mocha -t 30000 test/acceptance/acceptance.js || {  echo "tests ${runtime} [build: ${build_num}] failed" ; result=1; }
+    cleanup
 }
 
+INITIAL_PWD=`pwd`
 build_num=$1
 result=0
 
