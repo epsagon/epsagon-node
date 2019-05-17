18753b58ce13d476e3c558f7caccddcad72e542e
ranrib <ran@epsagon.com>
2019-05-17 16:18:47 +0300

    feat(frameworks): fork out frameworks

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const nodeWrapper = require('./wrappers/node.js');
 const batchWrapper = require('./wrappers/batch.js');
 const tracer = require('./tracer.js');
 const config = require('./config.js');
+const utils = require('./utils.js');
+const eventInterface = require('./event.js');
+const event = require('./proto/event_pb.js');
+const tryRequire = require('./try_require.js');
+const errorCode = require('./proto/error_code_pb.js');
 
 // Requiring patcher to instrument modules
 const patcher = require('./patcher.js'); // eslint-disable-line no-unused-vars
@@ -15,6 +20,13 @@ module.exports = {
     wrapBatchJob: f => f,
     label: f => f,
     setError: f => f,
+    tracer,
+    config,
+    utils,
+    eventInterface,
+    event,
+    tryRequire,
+    errorCode,
 };
 
 if (!config.getConfig().isEpsagonDisabled) {
