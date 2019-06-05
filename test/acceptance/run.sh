35b2e3289643dc66e2f48c5737c7fbb18bb4277c
LiadGoren <Liad@epsagon.com>
2019-06-05 18:15:03 +0300

    feat(http.js): support user-defined blacklist for http filtering

--- test/acceptance/run.sh
+++ test/acceptance/run.sh
@@ -3,7 +3,7 @@
 function cleanup() {
     echo 'cleaning up'
     cd /tmp/acceptance/lambda-handlers/
-    #serverless remove --runtime ${runtime} --runtimeName ${runtimeName} --buildNumber ${build_num}
+    serverless remove --runtime ${runtime} --runtimeName ${runtimeName} --buildNumber ${build_num}
     cd $INITIAL_PWD
     rm -rf /tmp/acceptance
 }
@@ -18,7 +18,7 @@ function run_acceptance_test() {
     cp -r test/acceptance/ /tmp/acceptance/
     cd /tmp/acceptance/lambda-handlers/
     npm install $INITIAL_PWD
-    #serverless deploy --runtime ${runtime} --runtimeName ${runtimeName} --buildNumber ${build_num} || {  echo "deployment of ${runtime} [build: ${build_num}] failed" ; result=1; }
+    serverless deploy --runtime ${runtime} --runtimeName ${runtimeName} --buildNumber ${build_num} || {  echo "deployment of ${runtime} [build: ${build_num}] failed" ; result=1; }
     cd -
     TRAVIS_BUILD_NUMBER=${build_num} RUNTIME=${runtime} RUNTIME_NAME=${runtimeName} mocha -t 30000 test/acceptance/acceptance.js || {  echo "tests ${runtime} [build: ${build_num}] failed" ; result=1; }
     cleanup
@@ -29,6 +29,6 @@ build_num=$1
 result=0
 
 run_acceptance_test ${build_num} nodejs10.x njs10
-#run_acceptance_test ${build_num} nodejs8.10 njs8
+run_acceptance_test ${build_num} nodejs8.10 njs8
 
 #exit ${result}
