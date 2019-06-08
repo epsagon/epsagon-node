ea32e1aaceaff98f26c161aedc2515be153dc819
Ran Ribenzaft <ranrib@users.noreply.github.com>
2019-06-08 13:30:45 +0300

    Create index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,23 @@
+// epsagon.d.ts
+declare module 'epsagon' {
+  import 'aws-lambda'
+
+  export function init(options: {
+    token: string
+    appName: string
+    metadataOnly?: boolean
+    useSSL?: boolean
+    traceCollectorURL?: string
+    isEpsagonDisabled?: boolean
+    urlPatternsToIgnore?: string[]
+  }): void
+  export function label(key: string, value: string): void
+  export function setError(error: Error): void
+
+  type Wrapper<T extends AWSLambda.Handler> = (f: T) => T
+
+  export function lambdaWrapper<T extends AWSLambda.Handler>(f: T): T
+  export function stepLambdaWrapper<T extends AWSLambda.Handler>(f: T): T
+  export function nodeWrapper<T extends Function>(f: T): T
+  export function wrapBatchJob<T extends Function>(f: T): T
+}
