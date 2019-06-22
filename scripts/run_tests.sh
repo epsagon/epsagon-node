f7c04e85696d1685fa4f82171fc87ab05363c9ef
ranrib <ran@epsagon.com>
2019-06-22 14:49:27 +0200

    fix(tests): ignore wsk in node 6

---
+++ scripts/run_tests.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+if [`node -v | cut -d '.' -f1` != 'v6']; then
+    mocha --recursive test/unit_tests --exclude test/unit_tests/wrappers/test_openwhisk.js
+else
+    mocha --recursive test/unit_tests
+fi
