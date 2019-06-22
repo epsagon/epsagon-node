b0e14337e904e52a6285640b9d80dab4299e0a83
ranrib <ran@epsagon.com>
2019-06-22 14:54:02 +0200

    fix(test): fix

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-if [`node -v | cut -d '.' -f1` != 'v6']; then
-    mocha --recursive test/unit_tests --exclude test/unit_tests/wrappers/test_openwhisk.js
-else
+if [ `node -v | cut -d '.' -f1,2` != 'v6' ]; then
     mocha --recursive test/unit_tests
+else
+    mocha --recursive test/unit_tests --exclude test/unit_tests/wrappers/test_openwhisk.js
 fi
