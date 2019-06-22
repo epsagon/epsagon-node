7eabe164bfffb99f3caaea83330ac7faa8e79034
ranrib <ran@epsagon.com>
2019-06-22 15:08:53 +0200

    fix(test): test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 if [ `node -v | cut -d '.' -f1,2` != 'v6' ]; then
+    echo "not 6"
     mocha --recursive test/unit_tests
 else
+    echo "6"
     mocha --recursive test/unit_tests --exclude test/unit_tests/wrappers/test_openwhisk.js
 fi
