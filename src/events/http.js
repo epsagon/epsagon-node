bd42fe85a1d69d1708724e0bf0b4216521edba09
Ran Ribenzaft <ranrib@users.noreply.github.com>
2019-11-03 12:45:09 +0200

    fix(http): support connections to container http endpoints (#186)
    This reverts commit e0edd4cfff6d13b20ed379eefafcec285f12182f.

--- src/events/http.js
+++ src/events/http.js
@@ -3,7 +3,6 @@
  */
 
 const uuid4 = require('uuid4');
-const uuidToHex = require('uuid-to-hex');
 const shimmer = require('shimmer');
 const http = require('http');
 const https = require('https');
@@ -157,14 +156,6 @@ function httpWrapper(wrappedFunction) {
                 (options && options.headers) || {}
             );
 
-            // Inject header to support tracing over HTTP requests to opentracing monitored code
-            const traceId = uuid4();
-            const hexTraceId = uuidToHex(traceId);
-            const spanId = uuidToHex(uuid4()).slice(16);
-            const parentSpanId = uuidToHex(uuid4()).slice(16);
-
-            headers['epsagon-trace-id'] = `${hexTraceId}:${spanId}:${parentSpanId}:1`;
-
             if (isBlacklistURL(hostname, path)) {
                 utils.debugLog(`filtered blacklist hostname ${hostname}`);
                 return wrappedFunction.apply(this, [a, b, c]);
@@ -173,6 +164,14 @@ function httpWrapper(wrappedFunction) {
                 utils.debugLog(`filtered blacklist headers ${JSON.stringify(headers)}`);
                 return wrappedFunction.apply(this, [a, b, c]);
             }
+
+            // Inject header to support tracing over HTTP requests to opentracing monitored code
+            const traceId = uuid4();
+            const hexTraceId = uuidToHex(traceId);
+            const spanId = uuidToHex(uuid4()).slice(16);
+            const parentSpanId = uuidToHex(uuid4()).slice(16);
+
+            headers['epsagon-trace-id'] = `${hexTraceId}:${spanId}:${parentSpanId}:1`;
 
             const agent = (
                 // eslint-disable-next-line no-underscore-dangle
@@ -221,10 +220,6 @@ function httpWrapper(wrappedFunction) {
                     request_headers: headers,
                     request_body: body,
                 });
-
-            eventInterface.addToMetadata(httpEvent, {
-                http_trace_id: traceId,
-            });
 
             const patchedCallback = (res) => {
                 const { isWreck } = ((options || {}).agent || {});
