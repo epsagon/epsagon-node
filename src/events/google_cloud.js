3056ad23cc3a06c1c1928d6ae76ce6ee47d654d7
Ido Nava <idonava@epsagon.com>
2020-01-07 14:29:32 +0200

    feat(pubsub): adding google pubsub instrumentation

--- src/events/google_cloud.js
+++ src/events/google_cloud.js
@@ -1,8 +1,11 @@
 /**
  * @fileoverview Instrumentation for google cloud library.
  */
+const { PubSub, v1 } = require('@google-cloud/pubsub');
+const shimmer = require('shimmer');
 const uuid4 = require('uuid4');
 const utils = require('../utils.js');
+const { initialEvent, finalizeEvent } = require('../helpers/events');
 const tracer = require('../tracer.js');
 const serverlessEvent = require('../proto/event_pb.js');
 const eventInterface = require('../event.js');
@@ -11,6 +14,13 @@ const moduleUtils = require('./module_utils.js');
 
 const URL_SPLIT_STRING = 'googleapis.com/';
 const BIG_QUERY = 'bigquery';
+
+const GOOGLE_CLOUD_TYPES = {
+    defaultProjectId: '{{projectId}}',
+    pubsub: {
+        name: 'pubsub',
+    },
+};
 
 /**
  * Wraps the bigQuery makeRequest function.
@@ -79,6 +89,132 @@ function bigQueryWrapper(wrappedFunction) {
     };
 }
 
+const getLastSplittedString = (array) => {
+    const splittedArray = (array && array.split('/')) || [];
+    return splittedArray[splittedArray.length - 1];
+};
+
+/**
+ * Wrap pubsub request function.
+ * @param {Function} original pubsub request function.
+ * @returns {Function} The wrapped function
+ */
+function wrapPubSubRequestFunction(original) {
+    return function internalPubSubRequestFunction(config, callback) {
+        if (!config || !config.method) {
+            return original.apply(this, [config, callback]);
+        }
+        let patchedCallback = callback;
+        try {
+            const pubsubProjectId = this.projectId;
+            const { event, startTime } = initialEvent(
+                GOOGLE_CLOUD_TYPES.pubsub.name, pubsubProjectId, config.method
+            );
+            const requestFunctionThis = this;
+            const responsePromise = new Promise((resolve) => {
+                patchedCallback = (err, arg2, arg3) => {
+                    if ((!pubsubProjectId || pubsubProjectId === GOOGLE_CLOUD_TYPES
+                        .defaultProjectId) && !!requestFunctionThis.projectId) {
+                        event.getResource().setName(requestFunctionThis.projectId);
+                    }
+                    const callbackResponse = {};
+                    if (arg2) {
+                        if (config.method === 'publish') {
+                            const messageIds = arg2.messageIds ? arg2.messageIds : arg2;
+                            if (Array.isArray(messageIds) && messageIds.length) {
+                                callbackResponse.messageIds = messageIds;
+                                const [messageId] = messageIds;
+                                callbackResponse.messageId = messageId;
+                            }
+                        } else if (config.method === 'createSubscription') {
+                            callbackResponse.subscription = arg2;
+                        } else if (config.method === 'deleteSubscription') {
+                            if (config.reqOpts && config.reqOpts.subscription) {
+                                callbackResponse.subscription = getLastSplittedString(
+                                    config.reqOpts.subscription
+                                );
+                            }
+                        } else if (config.method === 'deleteTopic') {
+                            if (config.reqOpts && config.reqOpts.topic) {
+                                callbackResponse.topic = getLastSplittedString(
+                                    config.reqOpts.topic
+                                );
+                            }
+                        } else if (config.method === 'createTopic') {
+                            callbackResponse.topic = arg2;
+                        }
+                    }
+                    finalizeEvent(event, startTime, err, callbackResponse);
+                    resolve();
+                    if (callback) {
+                        if (arg3) {
+                            callback(err, arg2, ...arg3);
+                        } else {
+                            callback(err, arg2);
+                        }
+                    }
+                };
+            });
+            tracer.addEvent(event, responsePromise);
+        } catch (err) {
+            tracer.addException(err);
+        }
+        return original.apply(this, [config, patchedCallback]);
+    };
+}
+
+/**
+ * Wrap pubsub request function.
+ * @param {Function} original pubsub request function.
+ * @returns {Function} The wrapped function
+ */
+function wrapPubSubPullFunction(original) {
+    return function internalPubSubPullFunction(request, options, callback) {
+        try {
+            let pubsubProjectId;
+            if (request && request.subscription) {
+                const subscriptionSplited = request.subscription.split('/');
+                if (subscriptionSplited.length > 1) {
+                    [, pubsubProjectId] = subscriptionSplited;
+                }
+            }
+            const { event, startTime } = initialEvent(
+                GOOGLE_CLOUD_TYPES.pubsub.name, pubsubProjectId, 'Pull'
+            );
+            const patchedCallback = (err, res) => {
+                const callbackResponse = {};
+                if (res && res.receivedMessages) {
+                    const messageIds = res.receivedMessages.reduce((acc, current) => {
+                        acc.push(current.message.messageId);
+                        return acc;
+                    }, []);
+                    callbackResponse.messageIds = messageIds;
+                }
+                finalizeEvent(event, startTime, err, callbackResponse);
+            };
+            let responsePromise;
+            // in case callback has given from the client.
+            if (callback) {
+                responsePromise = original.apply(this, [request, options, patchedCallback]);
+                tracer.addEvent(event, responsePromise);
+            }
+            responsePromise = original.apply(this, [request, options, callback]);
+            tracer.addEvent(event, responsePromise);
+            return responsePromise.then((res) => {
+                const [response] = res;
+                patchedCallback(null, response);
+                return res;
+            }, (err) => {
+                patchedCallback(err, null);
+                throw err;
+            });
+        } catch (err) {
+            tracer.addException(err);
+        }
+        return original.apply(this, [request, options, callback]);
+    };
+}
+
 module.exports = {
     /**
      * Initializes the bigQuery makeRequest tracer
@@ -90,5 +226,9 @@ module.exports = {
             bigQueryWrapper,
             common => common.util
         );
+
+        shimmer.wrap(PubSub.prototype, 'request', () => wrapPubSubRequestFunction(PubSub.prototype.request));
+        shimmer.wrap(PubSub.prototype, 'subscription', () => wrapPubSubRequestFunction(PubSub.prototype.subscription));
+        shimmer.wrap(v1.SubscriberClient.prototype, 'pull', () => wrapPubSubPullFunction(v1.SubscriberClient.prototype.pull));
     },
 };
