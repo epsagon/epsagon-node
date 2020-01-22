2015ad37fd91fc5180854dc8056baed32c0ec879
ranrib <ran@epsagon.com>
2020-01-22 11:18:44 +0200

    fix(travis): fix build

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-if [ `node -v | cut -d '.' -f1` != 'v6' ]; then
-    mocha --recursive test/unit_tests
+if [[ `node -v | cut -d '.' -f1` =~ ^(v6|v8)$ ]]; then
+    mocha --recursive test/unit_tests --exclude test/unit_tests/wrappers/test_openwhisk.js --exclude test/unit_tests/wrappers/test_openwhisk_traces.js
 else
-    mocha --recursive test/unit_tests --exclude test/unit_tests/wrappers/test_openwhisk.js
+    mocha --recursive test/unit_tests
 fi
