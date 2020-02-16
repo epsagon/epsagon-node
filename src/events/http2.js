943bed27c8c151e6883c12ae808e9a97a9d47e32
ranrib <ran@epsagon.com>
2020-02-16 12:20:55 +0200

    feat(http2.js): add support for HTTP2

--- src/events/http2.js
+++ src/events/http2.js
@@ -20,7 +20,7 @@ const {
     updateAPIGateway,
 } = require('./http');
 const tryRequire = require('../try_require');
-const http2 = tryRequire('http22');
+const http2 = tryRequire('http2');
 
 
 /**
