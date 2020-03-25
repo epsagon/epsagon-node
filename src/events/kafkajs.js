b702d31f4e97242cf6ffb08b7aa216604173967f
Ran Ribenzaft <ranrib@users.noreply.github.com>
2020-03-25 10:54:55 +0200

    feat(kafkajs.js): add support for kafkajs (#247)

---
+++ src/events/kafkajs.js
@@ -0,0 +1,125 @@
+/**
+ * @fileoverview Handlers for kafkajs instrumentation
+ */
+
+const tracer = require('../tracer.js');
+const eventInterface = require('../event.js');
+const utils = require('../utils.js');
+const moduleUtils = require('./module_utils.js');
+const { EPSAGON_HEADER } = require('../consts.js');
+const { generateEpsagonTraceId } = require('../helpers/http');
+
+
+/**
+ * acts as a middleware for `producer.send()`
+ * @param {object} messages the messages param to send
+ * @param {Kafka} producer producer object
+ * @returns {Promise} The response promise
+ */
+function kafkaMiddleware(messages, producer) {
+    let result;
+    let originalHandlerAsyncError;
+    const epsagonId = generateEpsagonTraceId();
+    let kafkaEvent;
+    let startTime;
+    let response;
+    try {
+        const { slsEvent, startTime: eventStartTime } = eventInterface.initializeEvent(
+            'kafka',
+            messages.topic,
+            'produce',
+            'kafkajs'
+        );
+        kafkaEvent = slsEvent;
+        startTime = eventStartTime;
+
+        // eslint-disable-next-line no-param-reassign
+        messages.messages = messages.messages.map((message) => {
+            if (!message.headers) {
+                // eslint-disable-next-line no-param-reassign
+                message.headers = {};
+            }
+            // eslint-disable-next-line no-param-reassign
+            message.headers[EPSAGON_HEADER] = epsagonId;
+            return message;
+        });
+    } catch (err) {
+        tracer.addException(err);
+    }
+
+    try {
+        response = producer.originalSend(messages);
+    } catch (err) {
+        if (kafkaEvent) {
+            eventInterface.setException(kafkaEvent, err);
+            tracer.addEvent(kafkaEvent);
+        }
+        throw err;
+    }
+
+    response = response.then((res) => {
+        result = res;
+        return res;
+    }).catch((err) => {
+        originalHandlerAsyncError = err;
+        throw err;
+    }).finally(() => {
+        try {
+            if (!kafkaEvent) {
+                utils.debugLog('Could not initialize kafkajs, so skipping response.');
+                return;
+            }
+            eventInterface.finalizeEvent(
+                kafkaEvent,
+                startTime,
+                originalHandlerAsyncError,
+                {
+                    messages_count: messages.messages.length,
+                    [EPSAGON_HEADER]: epsagonId,
+                },
+                {
+                    messages,
+                    response: result,
+                }
+            );
+        } catch (err) {
+            tracer.addException(err);
+        }
+    });
+
+    if (kafkaEvent) {
+        tracer.addEvent(kafkaEvent, response);
+    }
+
+    return response;
+}
+
+
+/**
+ * Wraps the kafkajs producer creation
+ * @param {Function} wrappedFunction The kafkajs producer function
+ * @returns {Function} The wrapped function
+ */
+function kafkaWrapper(wrappedFunction) {
+    return function internalKafkaWrapper(options) {
+        const producer = wrappedFunction.apply(this, [options]);
+        const patchedSend = messages => kafkaMiddleware(messages, producer);
+        producer.originalSend = producer.send;
+        producer.send = patchedSend.bind(producer);
+        return producer;
+    };
+}
+
+module.exports = {
+    /**
+     * Initializes the kafkajs tracer
+     */
+    init() {
+        moduleUtils.patchModule(
+            'kafkajs',
+            'producer',
+            kafkaWrapper,
+            kafka => kafka.Kafka.prototype
+        );
+    },
+};
