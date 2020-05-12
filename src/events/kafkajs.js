ab25fbf8e030e1c5fe4df837a223151242830a7b
Ido Nava <idonava@epsagon.com>
2020-05-12 13:42:46 +0300

    fix(kafka.js): fix review

--- src/events/kafkajs.js
+++ src/events/kafkajs.js
@@ -94,81 +94,80 @@ function kafkaSendMiddleware(messages, producer) {
     return response;
 }
 /**
- * acts as a middleware for `producer.connect()`
- * @param {Kafka} producer producer object
+ * Wrap kafka cluster connect function`
+ * @param {Function} originalConnectFunction connect function
  * @returns {Promise} The response promise
  */
-function kafkaConnectMiddleware(producer) {
-    let originalHandlerAsyncError;
-    let kafkaEvent;
-    let startTime;
-    let response;
-    try {
-        const { slsEvent, startTime: eventStartTime } = eventInterface.initializeEvent(
-            'kafka',
-            'connect',
-            'produce',
-            'kafkajs'
-        );
-        kafkaEvent = slsEvent;
-        startTime = eventStartTime;
-    } catch (err) {
-        tracer.addException(err);
-    }
-    try {
-        response = producer.originalConnect();
-    } catch (err) {
-        if (kafkaEvent) {
-            kafkaEvent.setException(kafkaEvent, err);
-            tracer.addEvent(kafkaEvent);
-        }
-        throw err;
-    }
-
-    response = response.then(res => res).catch((err) => {
-        originalHandlerAsyncError = err;
-        throw err;
-    }).finally(() => {
+function kafkaConnectWrapper(originalConnectFunction) {
+    return function internalKafkaConnectWrapper(...args) {
+        let originalHandlerAsyncError;
+        let kafkaEvent;
+        let startTime;
+        let response;
+        let port;
         try {
-            if (!kafkaEvent) {
-                utils.debugLog('Could not initialize kafkajs, skipping response.');
-                return;
-            }
-            eventInterface.finalizeEvent(
-                kafkaEvent,
-                startTime,
-                originalHandlerAsyncError
+            const { host, port: brokerPort } = this.brokerPool.seedBroker.connection;
+            port = brokerPort;
+            const { slsEvent, startTime: eventStartTime } = eventInterface.initializeEvent(
+                'kafka_server',
+                host,
+                'connect',
+                'kafkajs'
             );
+            kafkaEvent = slsEvent;
+            startTime = eventStartTime;
         } catch (err) {
             tracer.addException(err);
         }
-    });
+        try {
+            response = originalConnectFunction.apply(this, args);
+        } catch (err) {
+            if (kafkaEvent) {
+                kafkaEvent.setException(kafkaEvent, err);
+                tracer.addEvent(kafkaEvent);
+            }
+            throw err;
+        }
+        response = response.then(res => res).catch((err) => {
+            originalHandlerAsyncError = err;
+            throw err;
+        }).finally(() => {
+            try {
+                if (!kafkaEvent) {
+                    utils.debugLog('Could not initialize kafkajs, skipping response.');
+                    return;
+                }
+                eventInterface.finalizeEvent(
+                    kafkaEvent,
+                    startTime,
+                    originalHandlerAsyncError,
+                    { port }
+                );
+            } catch (err) {
+                tracer.addException(err);
+            }
+        });
 
-    if (kafkaEvent) {
-        tracer.addEvent(kafkaEvent, response);
-    }
+        if (kafkaEvent) {
+            tracer.addEvent(kafkaEvent, response);
+        }
 
-    return response;
+        return response;
+    };
 }
-
 
 /**
  * Wraps the kafkajs producer creation
  * @param {Function} wrappedFunction The kafkajs producer function
  * @returns {Function} The wrapped function
  */
-function kafkaWrapper(wrappedFunction) {
-    return function internalKafkaWrapper(options) {
+function kafkaProducerWrapper(wrappedFunction) {
+    return function internalKafkaProducerWrapper(options) {
         // patching send request.
         const producer = wrappedFunction.apply(this, [options]);
         const patchedSend = messages => kafkaSendMiddleware(messages, producer);
         producer.originalSend = producer.send;
         producer.send = patchedSend.bind(producer);
-        // patching connect request.
-        const patchedConnect = () => kafkaConnectMiddleware(producer);
-        producer.originalConnect = producer.connect;
-        producer.connect = patchedConnect.bind(producer);
-
         return producer;
     };
 }
@@ -181,8 +180,14 @@ module.exports = {
         moduleUtils.patchModule(
             'kafkajs',
             'producer',
-            kafkaWrapper,
+            kafkaProducerWrapper,
             kafka => kafka.Kafka.prototype
+        );
+        moduleUtils.patchModule(
+            'kafkajs/src/cluster/index.js',
+            'connect',
+            kafkaConnectWrapper,
+            cluster => cluster.prototype
         );
     },
 };
