30ba7d122ea0a2ab96a0378020a3154baf69d563
ranrib <ran@epsagon.com>
2020-05-15 00:46:41 +0300

    feat(openwhisk.js): capture namespace and host in openwhisk

--- src/events/openwhisk.js
+++ src/events/openwhisk.js
@@ -32,6 +32,9 @@ function openWhiskWrapper(wrappedFunction) {
 
         invokeEvent.setResource(resource);
         eventInterface.addToMetadata(invokeEvent, {
+            api_host: options.apihost || process.env['__OW_API_HOST'],
+            namespace: options.namespace || process.env['__OW_NAMESPACE'],
+        }, {
             params: options.params,
         });
         let request;
@@ -69,7 +72,6 @@ function openWhiskWrapper(wrappedFunction) {
                     invokeEvent,
                     brief,
                     {
-                        activation_id: res.activationId,
                         response: resp,
                     }
                 );
