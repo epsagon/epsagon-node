6df56891311392dab3806f0874013c0513776271
Ido Nava <idonava@epsagon.com>
2020-11-16 11:01:53 +0200

    feat(ec2): fix review

--- src/containers/ec2.js
+++ src/containers/ec2.js
@@ -6,6 +6,8 @@ let currentEC2Labels = null;
 
 const URL = 'http://169.254.169.254/latest/meta-data/';
 const attributeToGet = ['instance-id', 'instance-type', 'local-ipv4', 'public-hostname', 'public-ipv4'];
+const EPSAGON_EC2_REQUEST_TIMEOUT = process.env.EPSAGON_EC2_REQUEST_TIMEOUT || 3000;
+
 
 /**
  * Load EC2 metadata and store it
@@ -13,14 +15,23 @@ const attributeToGet = ['instance-id', 'instance-type', 'local-ipv4', 'public-ho
  */
 module.exports.loadEC2Metadata = function loadEC2Metadata() {
     if (currentEC2Labels) return Promise.resolve(currentEC2Labels);
+
+
     const promises = [];
     utils.debugLog('Loading EC2 metadata');
     attributeToGet.forEach((attribute) => {
-        promises.push(axios.get(URL + attribute).then((response) => {
+        const source = axios.CancelToken.source();
+        setTimeout(() => {
+            source.cancel();
+        }, EPSAGON_EC2_REQUEST_TIMEOUT);
+        promises.push(axios.get(URL + attribute, {
+            timeout: EPSAGON_EC2_REQUEST_TIMEOUT,
+            cancelToken: source.token,
+        }).then((response) => {
             utils.debugLog(`Received response for ${attribute}: ${response.data}`);
             if (response.status === 200) {
                 if (!currentEC2Labels) currentEC2Labels = {};
-                currentEC2Labels[`ec2.${attribute}`] = response.data;
+                currentEC2Labels[`aws.ec2.${attribute}`] = response.data;
                 utils.debugLog(`Received ${attribute}: ${attribute}`);
             }
             return attribute;
