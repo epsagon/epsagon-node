65f74a06cdb37bb9db986156c281993193a3efb2
Tal Vintrob <tal@epsagon.com>
2021-07-05 17:20:44 +0300

    wip

--- src/events/node_redshift.js
+++ src/events/node_redshift.js
@@ -1,5 +1,10 @@
+const uuid4 = require('uuid4');
+const utils = require('../utils.js');
+const errorCode = require('../proto/error_code_pb.js');
 const moduleUtils = require('./module_utils.js');
-const sqlWrapper = require('./sql.js');
+const serverlessEvent = require('../proto/event_pb.js');
+const tracer = require('../tracer.js');
+const eventInterface = require('../event.js');
 
 /**
  * Wraps Redshift's runQuery with tracing
@@ -8,16 +13,49 @@ const sqlWrapper = require('./sql.js');
  */
 function nodeRedshiftWrapper(wrappedFunction) {
     return function internalRedshiftRunQueryWrapper(...args) {
-        debugger;
         const { config } = this;
+        const { host, port } = config;
         const argsCopy = [...args];
         const query = argsCopy.shift();
         const cb = argsCopy.pop();
 
-        const patchedCallback = sqlWrapper.wrapSqlQuery(query, undefined, cb, config, 'redshift');
+        const startTime = Date.now();
+
+        const resource = new serverlessEvent.Resource([
+            host,
+            'redis',
+            'query',
+        ]);
+
+        const dbapiEvent = new serverlessEvent.Event([
+            `redshift-${uuid4()}`,
+            utils.createTimestampFromTime(startTime),
+            null,
+            'redis',
+            0,
+            errorCode.ErrorCode.OK,
+        ]);
+
+        dbapiEvent.setResource(resource);
+
+        let patchedCB = cb;
+        const p = new Promise(resolve => {
+            patchedCB = (err, data) => {
+                dbapiEvent.setDuration(utils.createDurationTimestamp(startTime));
+
+                resolve();
+
+                if (cb) {
+                    cb(err, data);
+                }
+            }
+        });
+
+        // eventInterface.addToMetadata(dbapiEvent, { host, port }, { query });
+        tracer.addEvent(dbapiEvent, p);
 
         argsCopy.unshift(query);
-        argsCopy.push(patchedCallback);
+        argsCopy.push(patchedCB);
 
         return wrappedFunction.apply(this, argsCopy);
     };
