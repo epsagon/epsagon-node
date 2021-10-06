8ddf9dec9d4884dee1932ad6ccab0c550baf72d8
Tal Vintrob <tvaintrob@users.noreply.github.com>
2021-10-06 13:10:18 +0300

    feat(logging): added logging around k8s metadata collection (#523)
    * feat(logging): added some logging aroung k8s metadata collection
    
    * feat(logging): logging the k8s service host
    
    * feat(logging): added prefix to k8s logs
    
    * feat(logging): fixing lint issues
    
    * feat(logging): fixing lint issues

--- src/containers/k8s.js
+++ src/containers/k8s.js
@@ -7,31 +7,55 @@ let k8sHostname = null;
 let k8sContainerId = null;
 
 /**
+* log invocations of functions
+* @param {Function} fn  the function to log
+* @returns {Any} the result of the function
+*/
+function logInvocation(fn) {
+    return (...args) => {
+        utils.debugLog(`[K8S-LOGS] invoking function: ${fn && fn.name}, time: ${new Date().toUTCString()}`);
+        const returnValue = fn.apply(this, args);
+
+        utils.debugLog(`[K8S-LOGS] function: ${fn && fn.name} finished execution, result: ${returnValue}, time: ${new Date().toUTCString()}`);
+        return returnValue;
+    };
+}
+
+
+/**
  * @returns {boolean} true if the current process is running inside
  * a K8S container, false otherwise
  */
-module.exports.hasK8sMetadata = function hasK8sMetadata() {
+/* eslint-disable-next-line prefer-arrow-callback */
+module.exports.hasK8sMetadata = logInvocation(function hasK8sMetadata() {
+    utils.debugLog(`[K8S-LOGS] process.env.KUBERNETES_SERVICE_HOST: ${process.env.KUBERNETES_SERVICE_HOST}`);
     return !!(process.env.KUBERNETES_SERVICE_HOST);
-};
+});
 
 /**
  * Load K8S metadata and store it
  */
-module.exports.loadK8sMetadata = function loadK8sMetadata() {
+/* eslint-disable-next-line prefer-arrow-callback */
+module.exports.loadK8sMetadata = logInvocation(function loadK8sMetadata() {
     if (!k8sHostname) {
         k8sHostname = os.hostname();
     }
 
     if (!k8sContainerId) {
         try {
+            utils.debugLog('[K8S-LOGS] calling readFile on /proc/self/cgroup');
+
             const data = fs.readFileSync('/proc/self/cgroup');
             const firstLineParts = data.toString('utf-8').split('\n')[0].split('/');
+
             k8sContainerId = firstLineParts[firstLineParts.length - 1];
+
+            utils.debugLog('[K8S-LOGS] finished loading K8s metadata');
         } catch (err) {
             utils.debugLog('Error loading k8s container id - cannot read cgroup file', err);
         }
     }
-};
+});
 
 /**
  * If the current process is running under an ECS container,
@@ -40,7 +64,8 @@ module.exports.loadK8sMetadata = function loadK8sMetadata() {
  *
  * @param {Object} runner  runner object to add the metadata
  */
-module.exports.addK8sMetadata = function addK8sMetadata(runner) {
+/* eslint-disable-next-line prefer-arrow-callback */
+module.exports.addK8sMetadata = logInvocation(function addK8sMetadata(runner) {
     if (!runner || !k8sHostname) return;
     const payload = {
         is_k8s: true,
@@ -50,5 +75,7 @@ module.exports.addK8sMetadata = function addK8sMetadata(runner) {
         payload.k8s_container_id = k8sContainerId;
     }
 
+    utils.debugLog('[K8S-LOGS] adding K8s metadata to trace');
     eventIterface.addToMetadata(runner, payload);
-};
+    utils.debugLog('[K8S-LOGS] finished adding K8s metadata to trace');
+});
