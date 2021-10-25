88a89cfd3bdd164dc8ea664f7653452b7160788f
sagiv_ramrajkar <sagivram15193@gmail.com>
2021-10-25 13:55:34 +0300

    add debug log for label

--- src/event.js
+++ src/event.js
@@ -112,6 +112,9 @@ module.exports.addLabelToMetadata = function addLabelToMetadata(event, key, valu
     if (labelsJson.length <= consts.MAX_LABEL_SIZE) {
         event.getResource().getMetadataMap().set('labels', labelsJson);
     }
+    else {
+        utils.debugLog('Max label size exceeded for: ' + key);
+    }
 };
 
 /**
