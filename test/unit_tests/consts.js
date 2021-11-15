32624ee739d97a4986d698398189c0e6a1754ead
Nimrod Shlagman <shlagman@gmail.com>
2021-11-15 16:50:19 +0200

    Revert "fix(add-aws-sdk-v3): add test for publish sns (#538)"
    This reverts commit 79fa6bbd1acf9ec02cdd5898ec23b39957cc6d01.

--- test/unit_tests/consts.js
+++
@@ -1,7 +0,0 @@
-const consts = require('../../src/consts.js');
-
-
-module.exports.REGION = consts.REGION;
-module.exports.SNS_NON_EXISTING_ARN = 'arn:aws:sns:someregion-1:262118031369:SnsSimpleTopic';
-module.exports.MESSAGE = 'MESSAGE_TEXT';
-module.exports.SNS_APP_NAME = 'aws-sdk-v3-sns-test';
