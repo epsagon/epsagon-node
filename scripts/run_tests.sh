b53cc835ba38a61516bbdad0f8544da707a5f344
nemo <nimrod@epsagon.com>
2021-12-21 16:58:30 +0200

    fix(test): improve stability

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ `node -v | cut -d '.' -f1` =~ ^(v6|v8)$ ]]; then
-    mocha --recursive test/unit_tests --exclude test/unit_tests/wrappers/test_openwhisk.js --exclude test/unit_tests/wrappers/test_openwhisk_traces.js
+    mocha --timeout 3000 --recursive test/unit_tests --exclude test/unit_tests/wrappers/test_openwhisk.js --exclude test/unit_tests/wrappers/test_openwhisk_traces.js
 else
-    mocha --recursive test/unit_tests
+    mocha --timeout 3000 --recursive test/unit_tests
 fi
